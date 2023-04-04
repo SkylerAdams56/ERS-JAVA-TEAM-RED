1bb72fdc046c59db3deaa0220b4b32fe4b1039d9
Lynn M <lkfmclaugh@gmail.com>
2023-04-04 14:12:28 -0400

    completed Item class, ItemController, & ItemRepository

---
+++ src/main/java/com/group/project/red/team/item/ItemRepository.java
@@ -0,0 +1,7 @@
+package com.group.project.red.team.item;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ItemRepository extends CrudRepository<Item, Integer> {
+
+}
