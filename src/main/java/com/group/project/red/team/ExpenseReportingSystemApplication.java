1ae7b9f095ba0410e486f807c8d749eb6a849c25
Skyler Adams <skyler14adams@gmail.com>
2023-04-04 11:59:44 -0400

    initialized the project

---
+++ src/main/java/com/group/project/red/team/ExpenseReportingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.group.project.red.team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseReportingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseReportingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group/project/red/team/ExpenseReportingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.project.red.team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseReportingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
